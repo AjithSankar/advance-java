28c8844856be5f7c9a0d8bde74fe2028078e5e91
AjithSankar <mailajithkumars@gmail.com>
2025-02-04 21:49:08 +0530

    Java Hash Map Implementation

---
+++ src/dev/ak/collections/Employee.java
@@ -0,0 +1,53 @@
+package dev.ak.collections;
+
+import java.util.Objects;
+
+public class Employee {
+
+    private Integer id;
+    private String name;
+    private int age;
+
+    public Employee(Integer id, String name, int age) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return age == employee.age && Objects.equals(id, employee.id) && Objects.equals(name, employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
+    }
+}
---
+++ src/dev/ak/collections/HashMapExamples.java
@@ -0,0 +1,24 @@
+package dev.ak.collections;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HashMapExamples {
+
+    public static void main(String[] args) {
+
+        Map<Employee, String> employees = new HashMap<>();
+        Employee employee = new Employee(1, "Ajith", 27);
+        System.out.println("hashcode of employee1: " + employee.hashCode());
+        Employee employee2 = new Employee(1, "Ajith", 27);
+        System.out.println("hashcode of employee2: " + employee2.hashCode());
+
+        employees.put(employee,"Developer");
+
+        System.out.println(employees.get(employee2));
+
+    }
+
+
+}
